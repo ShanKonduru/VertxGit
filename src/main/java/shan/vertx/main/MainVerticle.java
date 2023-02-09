3e1ca0b5969c63ea18780b5f08b29a64b9eae97f
Shan Konduru <ShanKonduru@users.noreply.github.com>
2023-02-09 14:18:16 +0530

    removed comments

--- src/main/java/shan/vertx/main/MainVerticle.java
+++ src/main/java/shan/vertx/main/MainVerticle.java
@@ -6,10 +6,8 @@ import shan.vertx.verticle.FirstVerticle;
 public class MainVerticle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("i am in verticle main");
 		System.out.println("About to Deploy first verticle ");
-
 		Vertx vertx = Vertx.vertx();
 		vertx.deployVerticle(new FirstVerticle());
 	}
